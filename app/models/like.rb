90b871c8dd646d941f8e5501495cf941db1d0478
KseniaGovorun <ksena.govorun@gmail.com>
2024-09-15 17:02:08 +0000

    Add annotate

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :bigint           not null, primary key
+#  tweet_id   :bigint           not null
+#  user_id    :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Like < ApplicationRecord
   belongs_to :tweet
   belongs_to :user
