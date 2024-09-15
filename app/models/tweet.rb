0813df00e344125b354ce0b1a04fd1c9bb0ee51d
KseniaGovorun <88275015+KseniaGovorun@users.noreply.github.com>
2024-09-15 20:05:02 +0300

    Add annotate (#9)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  user_id    :bigint           not null
+#  origin_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Tweet < ApplicationRecord
   belongs_to :user
   belongs_to :origin, class_name: "Tweet", foreign_key: "origin_id"
