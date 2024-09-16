83484d886a8493382ebfac0c128091656db15a3a
KseniaGovorun <ksena.govorun@gmail.com>
2024-09-16 17:19:07 +0000

    Change test

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -13,13 +13,13 @@ require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
   context 'validations' do
-    it { should validate_length_of(:body).is_at_most(300) }
+    it { is_expected.to validate_length_of(:body).is_at_most(300) }
   end
 
   context 'associations' do
-    it { should belong_to(:user) }
-    it { should have_one(:origin) }                      # it { should have_one (:origin).with_foreign_key('origin_id') }
-    it { should have_many (:comments) }
-    it { should have_many (:likes) }
+    it { is_expected.to belong_to(:user) }
+    it { is_expected.to have_one(:origin) }                      # it { should have_one (:origin).with_foreign_key('origin_id') }
+    it { is_expected.to have_many (:comments) }
+    it { is_expected.to have_many (:likes) }
   end
 end
