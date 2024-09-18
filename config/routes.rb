1de217ce5cca575b0c5c38d50877e6c1126ccc38
KseniaGovorun <ksena.govorun@gmail.com>
2024-09-18 19:14:06 +0000

    Add views

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+   # Defines the root path route ("/")
+   devise_scope :user do
+    get "/sign_in", to: "devise/sessions#new"
+   end
 end
