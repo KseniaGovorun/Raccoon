8f828a10c67583b3a920e27dc9cc7e7bcd230aca
KseniaGovorun <ksena.govorun@gmail.com>
2024-09-19 14:25:19 +0000

    Necessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, only: [ :sessions, :registrations ]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,9 +9,4 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-   # Defines the root path route ("/")
-   devise_scope :user do
-    get "/sign_in", to: "devise/sessions#new"
-   end
 end
