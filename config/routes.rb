21c70a3d0bbc5bb75b66737bcf2d57857f5ccb2d
KseniaGovorun <ksena.govorun@gmail.com>
2024-09-20 09:03:42 +0000

    Add clases and username_field

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, only: [ :sessions, :registrations ]
+  devise_scope :user do
+    unauthenticated :user do
+      root to: "devise/registrations#new", as: :unauthenticated_root
+    end
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
