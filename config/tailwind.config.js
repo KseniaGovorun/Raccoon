733b27ede8d10b980257ab13cb905b4099e19faa
KseniaGovorun <ksena.govorun@gmail.com>
2024-09-20 16:17:06 +0000

    Add changes after review

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,6 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/assets/stylesheets/**/*.css',
     './app/views/**/*.{erb,haml,html,slim}'
   ],
   theme: {
