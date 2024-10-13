6e5f33e26bdfe3643de93a48889809843525de29
KseniaGovorun <ksena.govorun@gmail.com>
2024-10-13 11:57:51 +0000

    Set up devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, only: [ :sessions, :registrations ]
+  devise_for :users, only: [ :sessions, :registrations ], controllers: {
+    registrations: "users/registrations", sessions: "users/sessions"
+  }
   devise_scope :user do
     unauthenticated :user do
       root to: "public_tweets#index", as: :unauthenticated_root
