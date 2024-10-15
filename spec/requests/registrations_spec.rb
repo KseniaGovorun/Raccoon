8bad2f75baffad41ab66390a28ac5fa54871ff4d
KseniaGovorun <ksena.govorun@gmail.com>
2024-10-15 20:57:22 +0000

    Added tests

---
+++ spec/requests/registrations_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Users::RegistrationsController, type: :controller do
+  let!(:user) { create(:user) }
+
+  describe 'after_sign_up_path_for' do
+    it 'returns the tweets path' do
+      expect(controller.after_sign_up_path_for(user)).to eq(tweets_path)
+    end
+  end
+end
