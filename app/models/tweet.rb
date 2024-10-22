9f1e18bfc7ae58779f3f8967a60f65c299aacbf5
KseniaGovorun <ksena.govorun@gmail.com>
2024-10-22 09:51:38 +0000

    Changed tweets annotate

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,7 @@
 #  id         :bigint           not null, primary key
 #  body       :text
 #  user_id    :bigint           not null
-#  origin_id  :bigint           not null
+#  origin_id  :bigint         
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
